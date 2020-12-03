ca6068563803b5a91253e4769fe1a167e6de098f
Nuril Muslichin <nurilmuslichin16@gmail.com>
2020-12-03 07:25:07 +0700

    Splash Screeen

---
+++ app/src/main/java/com/example/moviecatalogue/ui/splash/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.moviecatalogue.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviecatalogue.R
+import com.example.moviecatalogue.ui.home.HomeActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        GlobalScope.launch {
+            delay(3000L)
+            val toHome = Intent(this@SplashScreen, HomeActivity::class.java)
+            startActivity(toHome)
+        }
+    }
+}
